67f3094e28c2254d7361717580a60f3103f03a2c
thynguyen <imthyins@gmail.com>
2020-03-14 22:52:13 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Slideshow from './components/slideshow/slideshow';
 
-export default App;
+const collection = [
+    { src: '/images/01.jpg', caption: "Caption one" },
+    { src: '/images/02.jpg', caption: "Caption two" },
+    { src: '/images/03.jpg', caption: "Caption three" },
+    { src: '/images/04.jpg', caption: "Caption four" },
+    { src: '/images/05.jpg', caption: "Caption five" },
+    { src: '/images/06.jpg', caption: "Caption six" },
+];
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Slideshow
+                    input={collection}
+                    ratio={`3:2`}
+                    mode={`automatic`}
+                    timeout={`3000`}
+                />
+            </div>
+        );
+    }
+}
---
+++ src/components/slideshow/slideshow.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './slideshow.scss'
+
+export default class Slideshow extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            slideIndex: 0
+        }
+
+        const ratioWHArray = this.props.ratio.split(":");
+        this.ratioWH = ratioWHArray[0] / ratioWHArray[1];
+    }
+
+    getNewSlideIndex = (step) => {
+        const slideIndex = this.state.slideIndex
+        const numberSlide = this.props.input.length
+
+        let newSlideIndex = slideIndex + step
+
+        if (newSlideIndex >= numberSlide) newSlideIndex = 0
+        else if (newSlideIndex < 0) newSlideIndex = numberSlide - 1
+
+        return newSlideIndex
+    }
+
+    backward = () => {
+        this.setState({
+          slideIndex: this.getNewSlideIndex(-1)
+        });
+      }
+    
+      forward = () => {
+        this.setState({
+          slideIndex: this.getNewSlideIndex(1)
+        });
+      }
+
+    setSlideIndex = (index) => {
+        this.setState({
+            slideIndex: index
+        })
+    }
+
+    updateDimensions = () => {
+        this.containerElm.style.height
+            = `${this.containerElm.offsetWidth / this.ratioWH}px`;
+    }
+
+    runAutomatic = () => {
+        this.setState({
+            slideIndex: this.getNewSlideIndex(1)
+        });
+    }
+
+    componentDidMount() {
+        this.rootElm = ReactDOM.findDOMNode(this);
+        this.containerElm = this.rootElm.querySelector(".slide-show__container");
+
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions);
+
+        if (this.props.mode === "automatic") {
+            const timeout = this.props.timeout || 5000;
+
+            this.automaticInterval = setInterval(
+                () => this.runAutomatic(),
+                Number.parseInt(timeout)
+            );
+        }
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+        if (this.automaticInterval) clearInterval(this.automaticInterval);
+    }
+
+    render() {
+        const owlClass = 'slide-show';
+        return (
+            <div className={`${owlClass}`}>
+                <div className={`${owlClass}__container`}>
+                    {
+                        this.props.input.map((image, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className={`${this.state.slideIndex === index ? `${owlClass}__container-slide--active` : ""} ${owlClass}__container-slide`}>
+                                    <div className={`${owlClass}__container-slide--number-text`}>
+                                        {`${index + 1} / ${this.props.input.length}`}
+                                    </div>
+                                    <img className={`${owlClass}__container-slide--image`} src={image.src} alt={image.caption} />
+                                    <div className={`${owlClass}__container-slide--caption-text`}>{image.caption}</div>
+                                </div>
+                            )
+                        })
+                    }
+
+                    <span className={`${owlClass}__container-slide--next`} onClick={this.forward}>❮</span>
+                    <span className={`${owlClass}__container-slide--prev`} onClick={this.backward}>❯</span>
+                </div>
+
+                <div className={`${owlClass}__dot-container`}>
+                    {
+                        this.props.input.map((_, index) => {
+                            return (
+                                <span
+                                    key={index}
+                                    className={`${owlClass}__dot-container--item ${this.state.slideIndex === index ? `${owlClass}__dot-container--item--active` : ""}`}
+                                    onClick={() => this.setSlideIndex(index)}
+                                >
+                                </span>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+}
