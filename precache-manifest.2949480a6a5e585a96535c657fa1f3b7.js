ec40e349d23b91be4a499e08a8b1b752c22f0131
thynguyen <imthyins@gmail.com>
2020-03-14 23:52:30 +0700

    Updates

---
+++ precache-manifest.2949480a6a5e585a96535c657fa1f3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c8b068558eb8fa2c0bfe1b2db8f148",
+    "url": "/thynguyen96/react_01-slide-show/index.html"
+  },
+  {
+    "revision": "e611d3efe35bd9b5d92c",
+    "url": "/thynguyen96/react_01-slide-show/static/css/main.d573e881.chunk.css"
+  },
+  {
+    "revision": "f87f475104b583823a74",
+    "url": "/thynguyen96/react_01-slide-show/static/js/2.7934c39e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/thynguyen96/react_01-slide-show/static/js/2.7934c39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e611d3efe35bd9b5d92c",
+    "url": "/thynguyen96/react_01-slide-show/static/js/main.19b01015.chunk.js"
+  },
+  {
+    "revision": "edb491f85d7a96063920",
+    "url": "/thynguyen96/react_01-slide-show/static/js/runtime-main.f1d36c02.js"
+  }
+]);
