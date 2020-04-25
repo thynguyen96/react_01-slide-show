a88bb0dd85e2c023cf110637cf55e68498754f5b
thynguyen <imthyins@gmail.com>
2020-04-25 19:54:35 +0700

    Updates

---
+++ precache-manifest.2d6ac1e65705e3274e8e9caadb5124d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b11f1dd5e0b8d74ff2f54c71b15fd7",
+    "url": "/react_01-slide-show/index.html"
+  },
+  {
+    "revision": "8db888dbc18cd0588190",
+    "url": "/react_01-slide-show/static/css/main.a96a35e8.chunk.css"
+  },
+  {
+    "revision": "379c891c05186adf8644",
+    "url": "/react_01-slide-show/static/js/2.834e42c7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react_01-slide-show/static/js/2.834e42c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db888dbc18cd0588190",
+    "url": "/react_01-slide-show/static/js/main.69b546aa.chunk.js"
+  },
+  {
+    "revision": "4618c31f07cf7ba9e54b",
+    "url": "/react_01-slide-show/static/js/runtime-main.5027f092.js"
+  }
+]);
